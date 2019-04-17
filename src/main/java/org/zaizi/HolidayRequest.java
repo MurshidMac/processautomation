28971eb5637f8027b31500868742c9e6d3d6d228
Murshid Hassen <mhassen@zaizi.com>
2019-04-17 15:43:40 +0530

    Getting the name is done

--- src/main/java/org/zaizi/HolidayRequest.java
+++ src/main/java/org/zaizi/HolidayRequest.java
@@ -2,7 +2,10 @@ package org.zaizi;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
 
 public class HolidayRequest {
     public static void main(String[] args) {
@@ -15,6 +18,20 @@ public class HolidayRequest {
                 .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
 
         ProcessEngine processEngine = cfg.buildProcessEngine();
-
+        
+        
+        RepositoryService respositoryservice = processEngine.getRepositoryService();
+        
+        Deployment deployment = respositoryservice.createDeployment()
+        		.addClasspathResource("holiday-request.bpmn20.xml")
+        		.deploy();
+        
+        ProcessDefinition processDefinition = respositoryservice.createProcessDefinitionQuery()
+        		  .deploymentId(deployment.getId())
+        		  .singleResult();
+        
+        System.out.println("Found process Definition: "+ processDefinition.getName());
+        
+        
     }
 }
