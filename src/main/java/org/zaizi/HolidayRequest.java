3093e7d4350832750004ad9fe7c7358455fc9241
Murshid Hassen <mhassen@zaizi.com>
2019-05-28 08:27:31 +0530

    last changes

--- src/main/java/org/zaizi/HolidayRequest.java
+++ src/main/java/org/zaizi/HolidayRequest.java
@@ -1,8 +1,11 @@
 package org.zaizi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class HolidayRequest {
     public static void main(String[] args) {
-
+        SpringApplication.run(HolidayRequest.class);
     }
 }
